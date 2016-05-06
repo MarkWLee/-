ca855dfde97ea69a341249fd5309957aebd31a91
李国华 <lakala@lakalademacbook-pro.local>
2016-05-06 10:43:41 +0800

    Initial Commit

---
+++ 转场动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  转场动画
+//
+//  Created by lakala on 16/5/6.
+//  Copyright © 2016年 MarkLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 转场动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  转场动画
+//
+//  Created by lakala on 16/5/6.
+//  Copyright © 2016年 MarkLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
